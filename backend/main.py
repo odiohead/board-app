8ffac171619af21ba610d56d420b2c39d409224c
odiohead <txpc03@gmail.com>
2026-03-02 15:59:07 +0900

    update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://3.34.53.69"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
